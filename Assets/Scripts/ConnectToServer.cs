8d2d0743eaba700675119d517ac78d13fee6bdad
Omran2222 <Omran2222@outlook.sa>
2022-10-01 00:06:36 +0300

    news

--- Assets/Scripts/ConnectToServer.cs
+++ Assets/Scripts/ConnectToServer.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Realtime;
 using Photon.Pun;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    
+    public TMP_InputField UsernameInput;
+    public TextMeshProUGUI ButtonText;
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
        
     }
 
-   public override void OnConnectedToMaster()
+    public void OnClickConnect()
+    {
+        if(UsernameInput.text.Length >= 1)
+        {
+            PhotonNetwork.NickName = UsernameInput.text;
+            ButtonText.text = "Connecting...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+     public override void OnConnectedToMaster()
     {
         PhotonNetwork.JoinLobby();
     }
-
+    
     public override void OnJoinedLobby()
     {
         SceneManager.LoadScene("Loading");
--- Assets/SpawnPlayers.cs
+++ Assets/SpawnPlayers.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class SpawnPlayers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject PlayerPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+void Start()
+{
+    Vector2 RandomPosition = new Vector2(Random.Range(minX,maxX),Random.Range(minY,maxY));
+    PhotonNetwork.Instantiate(PlayerPrefab.name,RandomPosition,Quaternion.identity);
+}
+    
 }
